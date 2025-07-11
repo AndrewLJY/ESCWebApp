839bde739994569b291eafaeaac8cb7d416679ca
Aik Kiat <millerman734@gmail.com>
2025-07-11 15:02:17 +0800

    latest

--- backend/routes/hotel.js
+++ backend/routes/hotel.js
@@ -1,19 +1,15 @@
 var express = require('express');
+const fs = require('fs');
 var router = express.Router();
 let jsonData; 
 
 async function loadJsonData(filePath) {
-  try {
-    const response = await fetch(filePath);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const jsonData = await response.json(); // Parse the JSON response
-    console.log(jsonData); // Work with the parsed JSON data
-    return jsonData;
-  } catch (error) {
-    console.error('Error fetching JSON:', error);
-  }
+    fs.readFile(filePath, 'utf-8', (error, jsonData) => {
+      if(error){
+        throw error;
+      }
+      return jsonData;
+    });
 }
 
 
@@ -37,13 +33,15 @@ router.get('/dest/:destination_name', async function(req, res, next){
     const data = await loadJsonData("../hotel_resources/destinations.json");
 
     const destination = req.params.destination_name;
+    destination.replace("_"," ");
+    
     if (!data){
           res.status(500).json({error:'unable to load json data'})
     }
     const hotelData = await GetHotels(destination,data);
-    if (!hotelData){
-          res.status(500).json({error:`unable to get hotel data of ${destination_name}`});
-    }
+    // if (!hotelData){
+    //       res.status(500).json({error:`unable to get hotel data of ${destination_name}`});
+    // }
     res.json(hotelData); //respond to user
 });
 
