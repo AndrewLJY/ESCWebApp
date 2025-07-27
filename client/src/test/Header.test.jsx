9db7b8fbef51075fc86bac205c21f29d48865aa7
Joaquim S. Gopu <joaquimgopu3@gmail.com>
2025-07-27 15:00:26 +0800

    confirmed password

--- client/src/test/Header.test.jsx
+++ client/src/test/Header.test.jsx
@@ -1,15 +1,17 @@
-//unittest
-
 import { render, screen, fireEvent } from "@testing-library/react";
 import Header from "../components/header";
 import React from "react";
+
+beforeAll(() => {
+  jest.spyOn(window, "alert").mockImplementation(() => {});
+});
 
 describe("Header Component", () => {
   test("Login dropdown opens on button click", () => {
     render(<Header />);
     const loginBtn = screen.getByText("Login");
     fireEvent.click(loginBtn);
-    expect(screen.getByText("Sign In Now")).toBeInTheDocument(); // to target button
+    expect(screen.getByText("Sign In Now")).toBeInTheDocument();
   });
 
   test("Signup dropdown appears when clicking 'Create one'", () => {
@@ -24,5 +26,25 @@ describe("Header Component", () => {
     fireEvent.click(screen.getByText("Login"));
     fireEvent.click(screen.getByText("Close"));
     expect(screen.queryByText(/Sign In/i)).not.toBeInTheDocument();
+  });
+
+  test("Shows alert when passwords do not match during signup", async () => {
+    render(<Header />);
+    fireEvent.click(screen.getByText("Login"));
+    fireEvent.click(screen.getByText(/Create one/i));
+
+    fireEvent.change(screen.getByLabelText("Email Address"), {
+      target: { value: "test@example.com" },
+    });
+    fireEvent.change(screen.getByLabelText("Password"), {
+      target: { value: "123456" },
+    });
+    fireEvent.change(screen.getByLabelText("Confirm Password"), {
+      target: { value: "654321" },
+    });
+
+    fireEvent.click(screen.getByText(/Sign Up Now/i));
+
+    expect(window.alert).toHaveBeenCalledWith("Passwords do not match!");
   });
 });
