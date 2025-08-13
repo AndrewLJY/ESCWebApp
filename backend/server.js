999506c423888ee48b2196351406087943ed15b3
AndrewLJY <andrewloke9@gmail.com>
2025-08-13 14:02:54 +0800

    port fix

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+// const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -63,8 +63,8 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
 
 module.exports = app;
